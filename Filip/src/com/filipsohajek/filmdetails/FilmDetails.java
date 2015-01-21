ec20ab2c517fda02a1db0bcc9bd89488d893e3e9
Filip Sohajek <filip.sohajek@gmail.com>
2015-01-21 18:59:01 +0100

    Added code for downloading data

--- Filip/src/com/filipsohajek/filmdetails/FilmDetails.java
+++ Filip/src/com/filipsohajek/filmdetails/FilmDetails.java
@@ -1,9 +1,37 @@
 package com.filipsohajek.filmdetails;
 
 
-public class FilmDetails {
-    public static void main(String args[])
-    {
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FilmDetails {
+    public static void main(String args[]) throws IOException {
+        int fid = 227786;
+        String surl = "http://csfdapi.cz/movie/" + String.valueOf(fid);
+        URL url = new URL(surl);
+        HttpURLConnection con = null;
+        InputStream is;
+        try {
+            con = (HttpURLConnection) url.openConnection();
+            is = con.getInputStream();
+        } catch (IOException e) {
+            System.err.println("Unknown I/O error! Dying...");
+            con.disconnect();
+            return;
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        String json = out.toString();
     }
 }
