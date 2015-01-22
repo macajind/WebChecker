aa507b4f9144dabd705e34c19de84c3f935378ee
Filip Sohajek <filip.sohajek@gmail.com>
2015-01-22 15:20:37 +0100

    Added basic output parsing and output

--- Filip/src/com/filipsohajek/filmdetails/FilmDetails.java
+++ Filip/src/com/filipsohajek/filmdetails/FilmDetails.java
@@ -12,9 +12,21 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FilmDetails {
+
     public static void main(String args[]) throws JSONException, MalformedURLException {
+        Map<String, String> authorslex = new HashMap<String, String>();
+        authorslex.put("directors", "Režiséři");
+        authorslex.put("original", "Námět");
+        authorslex.put("script", "Scénář");
+        authorslex.put("camera", "Kamera");
+        authorslex.put("soundtrack", "Hudba");
+        authorslex.put("actors", "Herci");
+
         int fid = 227786;
         String surl = "http://csfdapi.cz/movie/" + String.valueOf(fid);
         URL url = new URL(surl);
@@ -38,6 +50,39 @@ public class FilmDetails {
         } catch (IOException e) {
             System.err.println("Unknown I/O error while reading response from server! Dying...");
         }
-      
+        String json = out.toString();
+        JSONObject jsonobj = new JSONObject(json);
+        JSONObject names = jsonobj.getJSONObject("names");
+        Iterator<?> namekeys = names.keys();
+        while(namekeys.hasNext()) {
+            String key = (String) namekeys.next();
+            String value = names.getString(key);
+            System.out.println("Název (" + key + ") : " + value);
+        }
+
+        System.out.println("\nRok vydání : " + String.valueOf(jsonobj.getInt("year")) + "\n");
+        System.out.println("Délka : " + jsonobj.getString("runtime") + "\n");
+        System.out.println("Žánry : ");
+
+        JSONArray genres = jsonobj.getJSONArray("genres");
+        for (int i = 0; i < genres.length(); i++) {
+            System.out.println(genres.getString(i));
+        }
+        System.out.println("\nPopis : " + jsonobj.getString("plot") + "\n");
+        System.out.println("\nHodnocení uživatelů : " + jsonobj.getInt("rating") + " %\n");
+
+        JSONObject authors = jsonobj.getJSONObject("authors");
+        Iterator<?> authorkeys = authors.keys();
+        while(authorkeys.hasNext()) {
+            String key = (String) authorkeys.next();
+            JSONArray value = authors.getJSONArray(key);
+            System.out.println(authorslex.get(key) + " : ");
+            for (int i = 0; i < value.length(); i++) {
+                System.out.println(value.getJSONObject(i).getString("name"));
+            }
+            System.out.print("\n");
+        }
+
     }
+
 }
