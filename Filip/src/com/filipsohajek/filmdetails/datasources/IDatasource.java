3799aa22447d4196a0bacf56f9d8eb256ed9ee1f
Filip Sohajek <filip.sohajek@gmail.com>
2015-01-22 21:29:18 +0100

    Abstraction 3/4: Created default datasource and renderer

--- Filip/src/com/filipsohajek/filmdetails/datasources/IDatasource.java
+++ Filip/src/com/filipsohajek/filmdetails/datasources/IDatasource.java
@@ -1,8 +1,12 @@
 package com.filipsohajek.filmdetails.datasources;
 
 import com.filipsohajek.filmdetails.Film;
+import com.filipsohajek.filmdetails.datasources.exceptions.FilmNotFoundException;
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public interface IDatasource {
-    public Film getFilmByName(String name);
-    public Film getFilmById(String id);
+    public Film getFilmByName(String name) throws IOException, FilmNotFoundException, JSONException;
+    public Film getFilmById(String id) throws IOException, JSONException;
 }
---
+++ Filip/src/com/filipsohajek/filmdetails/renderers/ConsoleRenderer.java
@@ -0,0 +1,101 @@
+package com.filipsohajek.filmdetails.renderers;
+
+import com.filipsohajek.filmdetails.Film;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ConsoleRenderer implements IRenderer {
+
+    HashMap<String, String> authorlex = new HashMap<String, String>();
+
+    Film film;
+
+    public ConsoleRenderer(Film film)
+    {
+        this.film = film;
+        authorlex.put("directors", "Režie");
+        authorlex.put("original", "Námět");
+        authorlex.put("script", "Scénář");
+        authorlex.put("camera", "Kamera");
+        authorlex.put("soundtrack", "Hudba");
+        authorlex.put("actors", "Herci");
+    }
+
+    @Override
+    public void renderFilm() {
+        renderNames();
+        renderBirth();
+        renderGenres();
+        renderRuntime();
+        renderRating();
+        renderPlot();
+        renderAuthors();
+    }
+
+    @Override
+    public void renderBirth() {
+        System.out.println("Rok vydání : " + film.getBirthyear().toString() + "\n");
+        System.out.println();
+    }
+
+    @Override
+    public void renderNames() {
+        HashMap<String, String> names = film.getNames();
+        Iterator iter = names.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry pairs = (Map.Entry) iter.next();
+            System.out.println("Jméno (" + pairs.getKey() + ") : " + pairs.getValue());
+            iter.remove();
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void renderPlot() {
+        System.out.println(film.getPlot());
+        System.out.println();
+    }
+
+    @Override
+    public void renderGenres() {
+        System.out.println("Žánry : ");
+        ArrayList<String> genres = film.getGenres();
+        for (String s : genres)
+        {
+            System.out.println(s);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void renderRating() {
+        System.out.println("Hodnocení : " + String.valueOf(film.getRating()));
+        System.out.println();
+    }
+
+    @Override
+    public void renderRuntime() {
+        System.out.println("Délka : " + String.valueOf(film.getRuntime()));
+        System.out.println();
+    }
+
+    @Override
+    public void renderAuthors() {
+        HashMap<String, ArrayList<String>> authors = film.getAuthors();
+        Iterator iter = authors.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry pairs = (Map.Entry) iter.next();
+            System.out.println(authorlex.get(pairs.getKey()) + " : ");
+            for (String s : (ArrayList<String>) pairs.getValue())
+            {
+                System.out.println(s);
+            }
+            iter.remove();
+            System.out.println();
+        }
+    }
+}
