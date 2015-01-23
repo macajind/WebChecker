695a2fb7c285c220d6bf415613545514554a34d5
Ondřej Štorc <o.storc@outlook.com>
2015-01-23 13:25:35 +0100

    First function version
    The first function version is here! I added a method of obtaining data
    from unpaired tags (but there are a few things that I need to upgrade
    ..) In the next commit I want to improve, collecting and removing HTML
    tags from text.

--- Ondřej/WebChecker/src/com/company/HtmlFile.java
+++ Ondřej/WebChecker/src/com/company/HtmlFile.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.ArrayList;
 
 /**
@@ -75,6 +77,25 @@ public class HtmlFile {
     }
 
     /**
+     * Get content of element's attribute
+     * @param element tag of element (<p>, <a href=''>, etc.) without brackets
+     * @param uniqueAttribute unique attribute, used to identify the element
+     * @param dataAttribute from which attribute you want to get data
+     * @return data of an attribute
+     * @trouble Data must be at end of the element.. :(
+     */
+    public String getContentOfAttribute(String element, String uniqueAttribute, String dataAttribute){
+        String searchElement = String.format("<%s %s %s=", element, uniqueAttribute, dataAttribute);
+        for (String line : file){
+            if(line.contains(searchElement)){
+                String temp = line.replace(line.substring(0, line.indexOf(dataAttribute)) + dataAttribute +"=\"", "");
+                temp = temp.substring(0, temp.indexOf('"'));
+                return temp;
+            }
+        }
+        return null;
+    }
+    /**
      * Calculate nesting of element, by counting '\t' on line
      * @param element element
      * @return nesting
@@ -88,5 +109,27 @@ public class HtmlFile {
                 break;
         }
         return nesting;
+    }
+
+    /**
+     * Remove element declaration around content. (From this: <p>Content</p> To this: Content)
+     * @param line line with elements which you want remove
+     * @return Line without element declaration
+     */
+    public String removeElement(String line){
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Remove element declaration around content. (From this: <p>Content</p> To this: Content)
+     * @param lines lines to process
+     * @return
+     */
+    public String[] removeElements(String[] lines){
+        ArrayList<String> processedLines = new ArrayList<String>();
+        for (String line : lines){
+            processedLines.add(removeElement(line));
+        }
+        return processedLines.toArray(new String[]{});
     }
 }
--- Ondřej/WebChecker/src/com/company/Main.java
+++ Ondřej/WebChecker/src/com/company/Main.java
@@ -4,18 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 	    // Download file
-        HtmlFile htmlFile = new HtmlFile(DataDownloader.downloadTextFile("http://www.csfd.cz/film/2294-vykoupeni-z-veznice-shawshank/"));
+        //HtmlFile htmlFile = new HtmlFile(DataDownloader.downloadTextFile("http://www.csfd.cz/film/2294-vykoupeni-z-veznice-shawshank/"));
+        HtmlFile htmlFile = new HtmlFile(DataDownloader.downloadTextFile("http://www.csfd.cz/film/2292-zelena-mile/"));
         // Write out file
-        writeOutStringArray(htmlFile.getFile());
-        System.out.println("\n\n\n\n\n");
-
-        // Get content of inline element <a href="... /a>
-        String[] test1 = htmlFile.getContentOfElement("a", "href=\"/zebricky/nejlepsi-filmy/?show=complete#highlight-chart-2294\"");
-        // Get content of element <div class="... /div>
-        String[] test2 = htmlFile.getContentOfElement("div", "class=\"ct-related similar\"");
-        writeOutStringArray(test1);
-        System.out.println("\n");
-        writeOutStringArray(test2);
+        System.out.println("Name: \t\t\t" + htmlFile.getContentOfAttribute("meta", "property=\"og:title\"", "content"));
+        System.out.print("Origin: \t\t");
+        writeOutStringArray(htmlFile.getContentOfElement("p", "class=\"origin\""));
+        System.out.print("Genre: \t\t\t");
+        writeOutStringArray(htmlFile.getContentOfElement("p", "class=\"genre\""));
+        System.out.println("Description:\n\t  " + htmlFile.getContentOfAttribute("meta", "name=\"description\"", "content").replace(".", ".\n\t"));
+        //region Method testing
+        /*    // Get content of inline element <a href="... /a>
+            String[] test1 = htmlFile.getContentOfElement("a", "href=\"/zebricky/nejlepsi-filmy/?show=complete#highlight-chart-2294\"");
+            // Get content of element <div class="... /div>
+            String[] test2 = htmlFile.getContentOfElement("div", "class=\"ct-related similar\"");
+            writeOutStringArray(test1);
+            System.out.println("\n");
+            writeOutStringArray(test2);
+            System.out.println("\n");
+            // Get content of content attribute
+            System.out.println(htmlFile.getContentOfAttribute("meta", "name=\"keywords\"", "content"));
+            // Get content of class attribute
+            System.out.println(htmlFile.getContentOfAttribute("a", "href=\"/film/289469-alcatraz/\"", "class"));*/
+        //endregion
     }
 
     /**
