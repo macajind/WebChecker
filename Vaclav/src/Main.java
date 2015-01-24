852f6593e0df9d5db6779856b64888703eb03c29
Václav košák <vaclav.kosak@gmail.com>
2015-01-24 17:39:21 +0100

    code modification
    code modification - Genres

--- Vaclav/src/Main.java
+++ Vaclav/src/Main.java
@@ -30,20 +30,23 @@ class Main {
         Element contributions = doc.select(selectorPagecontent).first();
         Elements selectedDivs = contributions.select(selectorPagecontent);
 
+
         String selectorName = "h1 span";
-        String selectorGenres = "div.infobar a, div.infobar a ";
+        String selectorGenres = "div.infobar a:gt(0)";
+        String selectorGenres2 = "div.infobar a:gt(2)";
         String selectorDate = "span.nobr a";
         String selectorTime = "div.txt-block time";
         String selectorStory = "div.canwrap p";
         for (Element div : selectedDivs) {
             Element name = div.select(selectorName).first();
             Element genres = div.select(selectorGenres).first();
+            Element genres2 = div.select(selectorGenres2).first();
             Element date = div.select(selectorDate).first();
             Element time = div.select(selectorTime).first();
             Element story = div.select(selectorStory).first();
             System.out.println("=====================================");
             System.out.printf("Name: %s \n", name.text());
-            System.out.printf("Genres: %s \n", genres.text());
+            System.out.printf("Genres: %s, %s \n", genres.text(), genres2.text());
             System.out.printf("Release date: %s \n", date.text());
             System.out.printf("Runtime: %s\n", time.text());
             System.out.printf("Storyline: %s \n", story.text());
