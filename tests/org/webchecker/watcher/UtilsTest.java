92fc6110652fb3fd5eb5f5776ddcd0d034631335
Jindřich Máca <macajind@fit.cvut.cz>
2015-03-26 21:01:11 +0100

    Delete of unnecessary class
    Test runner class is useless, because you can run tests in your IDE.
    
    - Uncommited test classes from previous revision

--- tests/org/webchecker/watcher/UtilsTest.java
+++ tests/org/webchecker/watcher/UtilsTest.java
@@ -3,9 +3,11 @@ package org.webchecker.watcher;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 /**
+ * Test case for support testing class {@link Utils}.
+ *
  * @author Matěj Kripner <kripnermatej@gmail.com>
  * @version 1.0
  */
